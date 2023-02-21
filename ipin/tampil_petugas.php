d6f4c20b5ffebf2dea60a571407350c9f67f1f8c
ippin <124118173+arifinwiranata@users.noreply.github.com>
2023-02-21 16:23:10 +0700

    Add files via upload

---
+++ ipin/tampil_petugas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Data Petugas</h1>
+
+    <table border="1">
+        <tr>
+            <td>no</td>
+            <td>Tanggal Pengaduan</td>
+            <td>NIK</td>
+            <td>isi laporan</td>
+            <td>foto</td>
+            <td>status</td>
+            <td>aksi</td>+
+        </tr>
+        <tr> 
+            <td>1</td>
+            <td>2023-02-23</td>
+            <td>1234</td>
+            <td>aaaaa</td>
+            <td>foto</td>
+            <td>0</td>
+            <td>
+                <button>detail</button> <button>update</button> <button>hapus</button>
+            </td>
+    </tr>
+    
+    </table>
+</body>
+
+</html>
